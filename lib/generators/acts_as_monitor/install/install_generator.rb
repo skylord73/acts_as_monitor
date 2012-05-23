d16308353f00cd84fad619aca4ec1be984082c6c
Skylord73 <skylord73@gmail.com>
2012-05-23 18:29:31 +0200

    update

--- lib/generators/acts_as_monitor/install/install_generator.rb
+++ lib/generators/acts_as_monitor/install/install_generator.rb
@@ -1,17 +1,33 @@
 require 'rails/generators'
 module ActsAsMonitor
-  module Generators
     class InstallGenerator < Rails::Generators::Base
-      desc "Install generatori for ActsAsMonitor gem"
+      desc "Install generator for ActsAsMonitor gem"
+      source_root File.expand_path("../templates", __FILE__)
+      puts("Path:#{__FILE__}")
       
-      def generate_install
-        puts("First method")
+      def copy_icons
+        icons = ["acts_as_monitor_green.png", "acts_as_monitor_red.png", "acts_as_monitor_yellow.png"]
+        icons.each do |icon| 
+          destination = "public/images/" + icon
+          copy_file icon, destination
+          puts("  Installed:   #{destination}")
+        end
       end
       
-      def generate_test
-        puts("Second method")
+      def copy_locales
+        file = "acts_as_monitor.it.yml"
+        destination = "config/locales/" + file
+          copy_file file, destination
+          puts("  Installed:   #{destination}")
+        end
       end
       
-    end
-  end
+      def print_usage
+        usage = <<-START
+        Add acts_as_monitor to model to monitor
+        Add a private metod warn_*? and error_*? to define warning and error status
+        Use monitor_tag(ModelClass) to view the output
+        START
+      end
+      
 end
